65dd3b5bdd9d64e5bada310023c587ee3a07c0e3
Katie Rodgers <katierodgers95@gmail.com>
2019-12-02 10:00:55 +0000

    experimenting with tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,55 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns"fizz" when passed 3' do
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 12' do
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 18' do
+    expect(fizzbuzz(18)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 20' do
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
